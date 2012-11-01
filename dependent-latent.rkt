27594797fd54d4f80cf5bab2cd7fa621ac93557d
Erik Silkensen <silkense@colorado.edu>
2012-10-31 21:59:02 -0400

    Initial commit

---
+++ dependent-latent.rkt
@@ -0,0 +1,51 @@
+#lang racket
+
+(require redex "base.rkt" "latent.rkt")
+
+(provide dep-λc ->lax-λc ->picky-λc)
+
+;; ----------------------------------------------------------------------------
+;; Syntax for dependent λc
+
+(define-extended-language dep-λc λc
+  [c .... (x : c ↦ c)]
+  [d T (c l l)]
+  [Γ ((x d) ...)])
+
+;; ----------------------------------------------------------------------------
+;; Operational semantics for dependent λc
+
+(define ->lax-λc
+  (extend-reduction-relation
+   ->λc dep-λc
+   [--> ((((x : c_1 ↦ c_2) l_p l_n) v_1) v_2)
+        (((subst x v_2 c_2) l_p l_n) (v_1 ((c_1 l_n l_p) v_2)))
+        "CDecompLax"]
+   [--> (in-hole E t_1) (in-hole E t_2)
+        (where (t_2) ,(apply-reduction-relation ->lax-λc (term t_1)))
+        "Compat"]))
+
+(define ->picky-λc
+  (extend-reduction-relation
+   ->λc dep-λc
+   [--> ((((x : c_1 ↦ c_2) l_p l_n) v_1) v_2)
+        (((subst x ((c_1 l_n l_p) v_2) c_2) l_p l_n) (v_1 ((c_1 l_n l_p) v_2)))
+        "CDecompPicky"]
+   [--> (in-hole E t_1) (in-hole E t_2)
+        (where (t_2) ,(apply-reduction-relation ->picky-λc (term t_1)))
+        "Compat"]))
+
+;; ----------------------------------------------------------------------------
+
+(module+ test
+  (define (test ->dep-λc t r)
+    (test-->> ->dep-λc t r))
+  
+  (define Pos (term {x : Int (pos x)}))
+  (define (f n)
+    (term (((g : (,Pos ↦ ,Pos) ↦ {z : Int (= z (g 0))}) "l_f" "l_g")
+           (λ (g : (Int -> Int)) (g ,n)))))
+  (define t1 (term (,(f 1) (λ (x : Int) 1))))
+  
+  (test ->lax-λc t1 1)
+  (test ->picky-λc t1 (term (⇑ "l_f"))))
